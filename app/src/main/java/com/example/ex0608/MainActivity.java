718856c545908a8dca5820f7d36331c5ca455aad
dy3722 <dy3722@bs.amalnet.k12.il>
2025-03-10 22:47:08 +0200

    finishing the desing

--- app/src/main/java/com/example/ex0608/MainActivity.java
+++ app/src/main/java/com/example/ex0608/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex0608;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv1up,tv1down,tv2up,tv2down,tv3up,tv3down;
+    EditText et1,et2,et3;
+    ImageView iv1,iv2,iv3;
+    Button btn1,btn2,btn3,btnNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
